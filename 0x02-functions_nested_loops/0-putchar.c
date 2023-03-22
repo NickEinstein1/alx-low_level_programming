8fabe6ace1c464b15f00278efe1f1cdfa9d2d085
NickEinstein1 <nickmwaurakaranu@gmail.com>
2023-03-21 20:11:11 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
  * main - print _putchar
  *
  * Return: 0
  */
-
 int main(void)
 {
 	_putchar('_');
@@ -17,5 +15,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
